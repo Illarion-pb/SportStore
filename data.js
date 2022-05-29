ba6fed44836670713594d81f8fca142203908bb6
Illarion Chesnokov <illarion.chesnokov@privatbank.ua>
2022-05-29 16:19:04 +0300

    Commit

---
+++ data.js
@@ -0,0 +1,16 @@
+module.exports = function () {
+    return {
+        products: [
+            {id: 1, name: "Каяк", category: "Водный спорт", description: "Лодка для одного человека", price: 275},
+            {id: 2, name: "Спасательный жилет", category: "Водный спорт", description: "Защитит и украсит", price: 48.95},
+            {id: 3, name: "Футбольный мяч", category: "Футбол", description: "Подтвержденный ФИФА размер и вес", price: 19.50},
+            {id: 4, name: "Угловой флажек", category: "Футбол", description: "Предаст вашему футбольному полю профессиноальный вид", price: 34.95},
+            {id: 5, name: "Стадион", category: "Футбол", description: "Складной стадион на 35000 мест", price: 79500},
+            {id: 6, name: "Думающая чашка", category: "Шахматы", description: "Увеличивает умственную активность на 75%", price: 16},
+            {id: 7, name: "Неученый стул", category: "Шахматы", description: "Заставляет соперника делать ошибки", price: 29.95},
+            {id: 8, name: "Шахматная доска", category: "Шахматы", description: "Отличная игра для всей семьи", price: 75},
+            {id: 9, name: "Крутые шахматные фигуры", category: "Шахматы", description: "Позолоченые с драг.камнями фигуры", price: 1200}
+        ],
+        orders: []
+    }
+}
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,8 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
+  selector: 'app',
+  template: '<div class="bg-success p-a-1 text-xs-center">This is SportStore</div>',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'app works!';
-}
+export class AppComponent {}
