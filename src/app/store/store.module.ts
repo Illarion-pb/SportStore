5feb26e9187737fefada4f5cedddfb2945e27f77
Illarion Chesnokov <illarion.chesnokov@privatbank.ua>
2022-06-02 07:32:45 +0300

    Commit

--- src/app/store/store.module.ts
+++ src/app/store/store.module.ts
@@ -4,10 +4,11 @@ import { FormsModule } from "@angular/forms";
 import { ModelModule } from "app/model/model.module";
 import { StoreComponent } from "./store.component";
 import { CounterDerective } from "./counter.directive";
+import { CartSummaryComponent } from "./cartSummary.component";
 
 @NgModule({
     imports: [ModelModule, BrowserModule, FormsModule],
-    declarations: [StoreComponent, CounterDerective],
+    declarations: [StoreComponent, CounterDerective, CartSummaryComponent],
     exports: [StoreComponent]
 })
 export class StoreModule {}
