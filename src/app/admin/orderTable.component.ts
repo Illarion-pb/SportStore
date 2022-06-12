5698911bb7b05b7560dc2ffc458e9dccafa176a6
Illarion Chesnokov <illarion.chesnokov@privatbank.ua>
2022-06-12 21:11:54 +0300

    commit

---
+++ src/app/admin/orderTable.component.ts
@@ -0,0 +1,12 @@
+import { Component } from "@angular/core";
+import { Product } from "app/model/product.model";
+import { ProductRepository } from "app/model/product.repository";
+
+@Component({
+    moduleId: module.id,
+    templateUrl: "productTable.component.html"
+})
+
+export class OrderTableComponent {
+    
+}
