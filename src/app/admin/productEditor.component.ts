c8a810f20ba2cded199412e8a76c13634c4736ad
Illarion Chesnokov <illarion.chesnokov@privatbank.ua>
2022-06-12 21:12:22 +0300

    Commit

---
+++ src/app/admin/productEditor.component.ts
@@ -0,0 +1,7 @@
+import { Component } from "@angular/core";
+
+@Component({
+    template: `<div class="bg-warning p-a-1">Product editor placeholder</div>`
+})
+
+export class ProductEditorComponent {}
